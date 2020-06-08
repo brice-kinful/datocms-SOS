92d545720f1f007c00b8fef8f3b3314cb373ca0d
Brice Christian <bricechristian93@gmail.com>
2020-06-08 14:21:40 -0400

    Added FAQs components

---
+++ src/components/faqs/question-answer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import parse from "html-react-parser";
+
+const QuestionAnswer = (props) => {
+  const item = props.content;
+  const { order } = props;
+  //   console.log(item);
+  return (
+    <section className={`q_a no-bottom-pad${order != 0 ? " no-top-pad" : ""}`}>
+      <div className="wrapper md">
+        <p className="question big-caslon-font text-xl center-text">
+          {item.question}
+        </p>
+        <div className="answer inner">{parse(item.answer)}</div>
+      </div>
+      <div className="border"></div>
+    </section>
+  );
+};
+
+export default QuestionAnswer;
--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -3,20 +3,28 @@ import { HelmetDatoCms } from "gatsby-source-datocms";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
+import QuestionAnswer from "../components/faqs/question-answer";
+
 const FaqsPage = ({ data }) => {
-  // const {page} = data.datoCmsFaqsPage;
+  const { title, page } = data.datoCmsFaqsPage;
+  // console.log(page);
 
   return (
     <Layout>
       <HelmetDatoCms />
-      <div id="FAQs" className="page">
+      <div id="faqs" className="page">
         <section className="hero pink-bg">
           <div className="wrapper center-text">
-            <h1 className="big-caslon-font text-3xl">
-              Frequently Asked Questions
-            </h1>
+            <h1 className="big-caslon-font text-3xl">{title}</h1>
           </div>
         </section>
+        {page.map((item, index) => {
+          return (
+            <React.Fragment key={item.id}>
+              <QuestionAnswer content={item} order={index} />
+            </React.Fragment>
+          );
+        })}
       </div>
     </Layout>
   );
@@ -28,6 +36,13 @@ export const query = graphql`
   query FaqsQuery {
     datoCmsFaqsPage {
       title
+      page: questionsAnswers {
+        ... on DatoCmsQA {
+          id
+          question
+          answer
+        }
+      }
     }
   }
 `;
